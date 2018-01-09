bd3fbd7d8707575add401d7658cfbbdf9978ec2e
Vladimir <34865131+Stacker007@users.noreply.github.com>
2018-01-09 20:39:40 +0300

    Create Task4.cpp

---
+++ Task4.cpp
@@ -0,0 +1,37 @@
+/*
+4. Ввести массив. Сформировать новый массив из отрицательных элементов первого
+массива.
+*/
+
+#include <iostream>
+#define SIZE 5 //размер массива
+
+using namespace std;
+void main() {
+	setlocale(LC_ALL, "rus");
+	int ourArr[SIZE];
+	int 
+	int sizeOfNew = 0;
+	for (int i=0 ; i < SIZE; i++ )		
+		{
+			cout << "Введите число № "<< i +1<< " ";
+			cin >> ourArr[i];
+		}
+	
+	cout << endl << "Исходный массив: ";
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << ourArr[i] << " ";
+	}
+	for (int i = 0, j = 0; i < SIZE; i++)
+		if (ourArr[i] < 0) sizeOfNew++;
+		
+
+	cout << endl << "В результате получается массив: ";
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout<< newArr[i]<< " ";
+	}
+	cout << endl;
+	system("pause");
+}
