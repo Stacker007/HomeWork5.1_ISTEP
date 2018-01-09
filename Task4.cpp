af15f9eebc3197e480a23c6912a5ba2ce40ed196
Vladimir <34865131+Stacker007@users.noreply.github.com>
2018-01-09 21:05:41 +0300

    FIXED

--- Task4.cpp
+++ Task4.cpp
@@ -10,27 +10,30 @@ using namespace std;
 void main() {
 	setlocale(LC_ALL, "rus");
 	int ourArr[SIZE];
-	int newArr[SIZE];
+	int newArr[SIZE] = {};
 	int sizeOfNew = 0;
-	for (int i=0 ; i < SIZE; i++ )		
-		{
-			cout << "Введите число № "<< i +1<< " ";
-			cin >> ourArr[i];
-		}
-	
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << "Введите число № " << i + 1 << " ";
+		cin >> ourArr[i];
+	}
+
 	cout << endl << "Исходный массив: ";
 	for (int i = 0; i < SIZE; i++)
 	{
 		cout << ourArr[i] << " ";
 	}
-	for (int i = 0, j = 0; i < SIZE; i++)
-		if (ourArr[i] < 0) sizeOfNew++;
-		
-
-	cout << endl << "В результате получается массив: ";
+	
 	for (int i = 0; i < SIZE; i++)
+		if (ourArr[i] < 0)
+		{
+			newArr[sizeOfNew] = ourArr[i];
+			sizeOfNew++;
+		}
+	cout << endl << "В результате получается массив: ";
+	for (int i = 0; i < sizeOfNew; i++)
 	{
-		cout<< newArr[i]<< " ";
+		cout << newArr[i] << " ";
 	}
 	cout << endl;
 	system("pause");
